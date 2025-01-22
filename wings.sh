d0d7f183c3d8a1266e31ff6fe8407ca538330cc3
thrkingunknown <131584252+thrkingunknown@users.noreply.github.com>
2025-01-22 19:54:05 +0530

    Update wings.sh

--- wings.sh
+++ wings.sh
@@ -20,10 +20,10 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 # Step 4: Update package list
-sudo apt update
+apt update
 
 # Step 5: Install Node.js and Git
-sudo apt install -y nodejs git
+apt install -y nodejs git
 
 # Step 6: Clone the repository
 git clone https://github.com/achul123/skyportd
