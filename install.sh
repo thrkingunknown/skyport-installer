df4c1508c618741681421d48ecf8537dadf56783
thrkingunknown <131584252+thrkingunknown@users.noreply.github.com>
2025-01-22 20:14:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,10 +16,10 @@ echo "Installing necessary dependencies...Subscribe To SoloPlayz!"
 mkdir -p /etc/apt/keyrings
 
 # Step 2: Add NodeSource GPG key
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
 # Step 3: Add NodeSource repository to sources list
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 # Step 4: Update package list
 apt update
